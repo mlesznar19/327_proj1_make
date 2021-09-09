93d493f4a023fe5412a63f65a08f08df57b3cf65
Michael Lesznar <michael.lesznar.19@cnu.edu>
2021-09-09 18:56:50 -0400

    adding h and cpp files

---
+++ myfunc.cpp
@@ -0,0 +1,17 @@
+/*
+ * myfunc.cpp
+ *
+ *  Created on: Sep 9, 2021
+ *      Author: michael
+ */
+
+#include "myfunc.h"
+
+std::string func()
+{
+	//say hello
+	return "hello";
+}
+
+
+
---
+++ myfunc.h
@@ -0,0 +1,17 @@
+/*
+ * myfunc.h
+ *
+ *  Created on: Sep 9, 2021
+ *      Author: michael lesznar
+ */
+
+
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+#include <string>
+
+std::string func();
+
+
+
+#endif /* MYFUNC_H_ */
